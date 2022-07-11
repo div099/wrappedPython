d66b724fc3f1b6d6558cb7a511e712e2bc54b74d
div099 <107322102+div099@users.noreply.github.com>
2022-07-11 16:23:47 -0400

    Update worker.js

--- worker.js
+++ worker.js
@@ -4,7 +4,15 @@ importScripts("https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js");
 async function loadPyodideAndPackages() { // loads pyodide
   self.pyodide = await loadPyodide(); // run the function and wait for the result (base library)
   await self.pyodide.loadPackage(["numpy", "pytz"]); // waits until these python packpages are loaded to continue
+
+  //import reductus library with micropip
+  await pyodide.runPythonAsync(`
+  import micropip
+  await micropip.install("reductus")
+  import reductus
+  `)
 }
+
 let pyodideReadyPromise = loadPyodideAndPackages(); // run the functions stored in lines 4
 
 self.onmessage = async (message) => { // when the worker.js receives a message...
@@ -15,13 +23,14 @@ self.onmessage = async (message) => { // when the worker.js receives a message..
 // pyodide.runPython is a syncronous function (returns when its finished automatically)
   const wrappedAdd = pyodide.runPython(`
   def add(a,b):
-      return a + b
+      return float(a) + float(b)
+  add
   `);
 
   try {
-    let results = wrappedAdd(message[0], message[1]); // calls the function so that the sum is returned
+    let results = wrappedAdd(message.data[0], message.data[1]); // calls the function so that the sum is returned
     self.postMessage(results); // waits to see if the code is completed
   } catch (error) {
-    self.postMessage ({ error: error.message, id }) // sends a message to script.js if there is an error
+    self.postMessage ({ error: error.message}) // sends a message to script.js if there is an error
   }
 };
